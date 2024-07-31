1c911a56839a4f59640b84a7ed23b356cfdf72bb
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-07-31 15:46:49 +0300

    Add files via upload

---
+++ classTxtFromInput.php
@@ -0,0 +1,22 @@
+<?php
+class Text extends Input
+{
+    private $placeholder;
+    public function getPlaceholder()
+    {
+        return $this->placeholder;
+    }
+    public function setPlaceholder($placeholder)
+    {
+        $this->placeholder = $placeholder;
+    }
+    public function __construct($background, $width, $height, $name, $value, $placeholder)
+    {
+        $this->setBackground($background);
+        $this->setWidth($width);
+        $this->setHeight($height);
+        $this->setName($name);
+        $this->setValue($value);
+        $this->setPlaceholder($placeholder);
+    }
+}
