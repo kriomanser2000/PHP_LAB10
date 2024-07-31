b625309ba6f3add19ce8dbef9c8b4570376b67ec
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-07-31 15:46:02 +0300

    Add files via upload

---
+++ classBtnFromInput.php
@@ -0,0 +1,22 @@
+<?php
+class Button extends Input
+{
+    private $isSubmit;
+    public function getSubmitState()
+    {
+        return $this->isSubmit;
+    }
+    public function setSubmitState($isSubmit)
+    {
+        $this->isSubmit = $isSubmit;
+    }
+    public function __construct($background, $width, $height, $name, $value, $isSubmit)
+    {
+        $this->setBackground($background);
+        $this->setWidth($width);
+        $this->setHeight($height);
+        $this->setName($name);
+        $this->setValue($value);
+        $this->setSubmitState($isSubmit);
+    }
+}
