729fe82e0143c14ce75be1d50d3fa301d7c4baa0
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-07-31 15:44:24 +0300

    Add files via upload

---
+++ classControl.php
@@ -0,0 +1,31 @@
+<?php
+class Control
+{
+    private $background;
+    private $width;
+    private $height;
+    public function getBackground()
+    {
+        return $this->background;
+    }
+    public function setBackground($background)
+    {
+        $this->background = $background;
+    }
+    public function getWidth()
+    {
+        return $this->width;
+    }
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+    public function getHeight()
+    {
+        return $this->height;
+    }
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+}
